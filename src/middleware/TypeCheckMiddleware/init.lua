701af42da75efb3292d1f02ad4dcc0a182bdd5ac
Jonathan <void.knight09@gmail.com>
2020-12-04 15:30:14 +1300

    updates to the type check middleware
    Now warns if an argument fails, with a bit of a more descriptive warning

--- src/middleware/TypeCheckMiddleware/init.lua
+++ src/middleware/TypeCheckMiddleware/init.lua
@@ -1,13 +1,23 @@
+local RunService = game:GetService("RunService")
+local HttpService = game:GetService("HttpService")
+local IS_STUDIO = RunService:IsStudio();
 local function typeCheckMiddleware(...)
     local checks = {...}
 
     -- The middleware function
-    return function(next)
+    return function(next, event)
+        local name = event:GetInstance().Name
+
         --  what's returned as callbackFn
         return function(player, ...)
             local args = {...}
             for index, check in ipairs(checks) do
                 if not check(args[index]) then
+                    if IS_STUDIO then
+                        warn("[TypeCheckMiddleware] Call to " .. name .. " failed")
+                        warn("\tRecieved: " .. HttpService:JSONEncode(args))
+                        warn("\tInvalid argument at index " .. tostring(index))
+                    end
                     return false
                 end
             end
