190c219ca0e02f73fcc54969f267a8530f013225
Jonathan <void.knight09@gmail.com>
2021-12-16 11:34:03 +1300

    Fixes

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,5 @@
 import { default as AsyncFunction } from "./ClientAsyncFunction";
 import { default as Event } from "./ClientEvent";
-import { default as GameMessagingEvent } from "./ClientMessagingEvent";
 import { default as Function } from "./ClientFunction";
 
-export { Event, AsyncFunction, GameMessagingEvent, Function };
+export { Event, AsyncFunction, Function };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import { default as AsyncFunction } from "./ServerAsyncFunction";
 import { default as Event } from "./ServerEvent";
-import { default as GameMessagingEvent } from "./ServerMessagingEvent";
+import { default as MessagingEvent } from "./ServerMessagingEvent";
 import { default as Function } from "./ServerFunction";
 
-export { Event, AsyncFunction, Function, GameMessagingEvent };
+export { Event, AsyncFunction, Function, MessagingEvent };
